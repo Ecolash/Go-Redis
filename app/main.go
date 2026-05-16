46cd51310cccbd3d5002d4522b981a7b0b4725b9
Ecolash <codeandcode0@gmail.com>
2026-05-17 00:26:20 +0530

    Update main

--- app/main.go
+++ app/main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+	"log"
+
+	"github.com/codecrafters-io/redis-starter-go/internal/server"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	redisServer, err := server.New("0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		log.Fatalf("failed to bind to port 6379: %v", err)
 	}
-
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	log.Println("Redis server listening on :6379")
+	redisServer.Run()
 }
