e835e22f63b8dd1da9c171a4af5363dc5344cd9e
Ecolash <codeandcode0@gmail.com>
2026-05-20 22:47:07 +0530

    Replication 2 / 3

--- internal/server/replication.go
+++ internal/server/replication.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
 
@@ -15,8 +17,37 @@ func (s *Server) handshakeWithMaster() {
 	}
 	defer conn.Close()
 
-	if _, err := conn.Write([]byte(resp.Array([]string{"PING"}))); err != nil {
-		log.Printf("replication: failed to send PING: %v", err)
+	r := bufio.NewReader(conn)
+
+	_, port, err := net.SplitHostPort(s.listener.Addr().String())
+	if err != nil {
+		log.Printf("replication: bad listener addr: %v", err)
 		return
 	}
+
+	steps := [][]string{
+		{"PING"},
+		{"REPLCONF", "listening-port", port},
+		{"REPLCONF", "capa", "psync2"},
+	}
+	for _, cmd := range steps {
+		if err := sendAndAwait(conn, r, cmd); err != nil {
+			log.Printf("replication: %s failed: %v", cmd[0], err)
+			return
+		}
+	}
+}
+
+func sendAndAwait(conn net.Conn, r *bufio.Reader, args []string) error {
+	if _, err := conn.Write([]byte(resp.Array(args))); err != nil {
+		return fmt.Errorf("write: %w", err)
+	}
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("read reply: %w", err)
+	}
+	if len(line) == 0 || line[0] == '-' {
+		return fmt.Errorf("unexpected reply %q", line)
+	}
+	return nil
 }
