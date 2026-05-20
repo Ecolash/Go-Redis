56dadfaa917012f140eb736735b938a621b9ecf1
Ecolash <codeandcode0@gmail.com>
2026-05-20 19:10:45 +0530

    Add --port option

--- app/main.go
+++ app/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/codecrafters-io/redis-starter-go/internal/server"
 )
 
 func main() {
-	redisServer, err := server.New("0.0.0.0:6379")
+	port := flag.Int("port", 6379, "port to listen on")
+	flag.Parse()
+
+	addr := fmt.Sprintf("0.0.0.0:%d", *port)
+	redisServer, err := server.New(addr)
 	if err != nil {
-		log.Fatalf("failed to bind to port 6379: %v", err)
+		log.Fatalf("failed to bind to port %d: %v", *port, err)
 	}
-	log.Println("Redis server listening on :6379")
+	log.Printf("Redis server listening on :%d", *port)
 	redisServer.Run()
 }
